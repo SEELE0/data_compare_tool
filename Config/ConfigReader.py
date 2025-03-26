ac2b06294faf548e8cc42ce890857bf24b8bb36b
todou <2314715137@qq.com>
2025-03-26 12:05:19 +0800

    ConfigReader.py load_excel方法实现 MainActuator.py 文件类型判断 PostProcessing.py

--- Config/ConfigReader.py
+++ Config/ConfigReader.py
@@ -1,12 +1,15 @@
 import json
+import pandas as pd
+
+
 class ConfigReader:
     def __init__(self, config_file, type='json'):
-        self.config_file = config_file # 配置文件路径
+        self.config_file = config_file  # 配置文件路径
         self.type = type
         self.config_data = {}
         self.load_config()
 
-    def load_config(self,type='json'):
+    def load_config(self, type='json'):
         if self.type == 'json':
             self.config_data = self.load_json()
         elif self.type == 'excel':
@@ -23,8 +26,13 @@ class ConfigReader:
             print(f"Error reading configuration file: {e}")
 
     def load_excel(self):
-        pass
-
+        try:
+            config_data = pd.read_excel(self.config_file).to_dict(orient='records')
+            return config_data
+        except FileNotFoundError:
+            print(f"Configuration file {self.config_file} not found.")
+        except Exception as e:
+            print(f"Error reading configuration file: {e}")
 
     def get(self, key):
         return self.config_data.get(key, None)
--- MainActuator.py
+++ MainActuator.py
@@ -5,6 +5,8 @@ from Config.ConfigReader import ConfigReader
 from DataSource.SourceCsv import SourceCsv
 from DataSource.Sourcefactory import Sourcefactory
 import pandas as pd
+
+
 # Factory = Source_Hub.create('Csv','./data/test.csv')
 
 class MainActuator:
@@ -24,24 +26,30 @@ class MainActuator:
 
     # 找出 df1 和 df2 中 主键都存在 但是值不相同的记录
 
-
     def main(self):
-        config_path = './Config/config.json'
+        # config_path = './Config/config.json'
 
-        # TYPE = input("请输入配置文件类型（json/excel）:\n"
-        #              "1.json\n"
-        #              "2.excel\n")
-        # if TYPE == '1':
-        #     config_path = './Config/config.json'
-        #     config_data = ConfigReader('config_path').config_data
+        TYPE = input("请输入配置文件类型（json/excel）:\n"
+                     "1.json\n"
+                     "2.excel\n")
+        if TYPE == '1':
+            config_path = './Config/config.json'
+            config_type = 'json'
+            # config_data = ConfigReader('config_path').config_data
+        elif TYPE == '2':
+            config_path = './Config/config.xlsx'
+            config_type = 'excel'
+        else:
+            print("请检查输入内容")
+            return
+
+        config_reader = ConfigReader(config_path, config_type)
+        config_data = config_reader.config_data
 
         Datafactory = Sourcefactory()
-        config_data = ConfigReader('config_path').config_data
+        # config_data = ConfigReader('config_path').config_data
         for config_row in config_data['sites']:
             df1, df2 = Datafactory.filter_same_colname(
-                Datafactory.create_entity(config_row['UpStream_Type'],config_row['filter']),
-                Datafactory.create_entity(config_row['DownStream_Type'],config_row['filter'])
+                Datafactory.create_entity(config_row['UpStream_Type'], config_row['filter']),
+                Datafactory.create_entity(config_row['DownStream_Type'], config_row['filter'])
             )
-
-
-
