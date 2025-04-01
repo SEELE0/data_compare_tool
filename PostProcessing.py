d886d6df49fad496ae89f5a987e6ad413143ccc1
todou <2314715137@qq.com>
2025-04-01 14:10:26 +0800

    PostProcessing.py

--- PostProcessing.py
+++ PostProcessing.py
@@ -1,19 +1,21 @@
 # 后处理类
-from MainActuator import MainActuator
 import pandas as pd
 
 
 class PostProcessing:
     @staticmethod
-    def process_missing_extract_records(df,type):
-        if type == 'missing_records': #下游系统数据丢失
-            pass
-        if type == 'extra_records': #上游系统数据丢失
-            pass
-        # PostProcessing.save_file(file_path)
+    def process_missing_extract_records(df, type, table_name):
+        if type == 'missing_records':  # 下游系统数据丢失
+            df['gap类型'] = '下游系统数据丢失'
+        elif type == 'extra_records':  # 上游系统数据丢失
+            df['gap类型'] = '上游系统数据丢失'
+        df['Source'] = ''
+        df['表名'] = table_name
+        PostProcessing.save_file(df)
+        del df
 
     @staticmethod
-    def process_diff_records(df, key_columns):
+    def process_diff_records(df, key_columns, table_name):
         # 读入 df 样例 其中 key_columns = 'name'  也就是主键为 name
         # 列名后带_df1 为上游数据   _df2为下游数据
 
@@ -27,11 +29,42 @@ class PostProcessing:
         # 数据存在差异  下游   member    zhou   ** 24 **            af
         # 数据存在差异  上游   member    kimi   ** 2 **          ** af **
         # 数据存在差异  下游   member    kimi   ** 0 **          ** na **
-        pass
-        # PostProcessing.save_file(file_path)
+
+        key_columns = key_columns.split(',')
+        result_rows = []
+        for _, row in df.iterrows():
+            key_values = row[key_columns]
+            df1_columns = [col for col in df.columns if col.endswith('_df1')]
+            df1_values = row[df1_columns].rename(lambda x: x.replace('_df1', ''))
+            df2_columns = [col for col in df.columns if col.endswith('_df2')]
+            df2_values = row[df2_columns].rename(lambda x: x.replace('_df2', ''))
+
+            # 处理上游数据
+            upstream = pd.concat([key_values, df1_values])
+            upstream['gap类型'] = '数据存在差异'
+            upstream['Source'] = '上游'
+            upstream['表名'] = table_name
+            result_rows.append(upstream)
+
+            # 处理下游数据
+            downstream = pd.concat([key_values, df2_values])
+            downstream['gap类型'] = '数据存在差异'
+            downstream['Source'] = '下游'
+            downstream['表名'] = table_name
+            result_rows.append(downstream)
+
+        result = pd.DataFrame(result_rows)
+        PostProcessing.save_file(result)
+        del df
+        del result
 
     @staticmethod
-    def save_file(self, file_path):
-        result = self.get_data()
-        result.to_csv(file_path, index=False)  ##### ⚠⚠⚠ 注意写入模式 ! 建议没个函数后都单独调用 save_file函数 及时释放内存
+    def save_file(df):
+        cols = df.columns.tolist()
+        cols.insert(0, cols.pop(cols.index('gap类型')))
+        cols.insert(1, cols.pop(cols.index('Source')))
+        cols.insert(2, cols.pop(cols.index('表名')))
+        df = df[cols]
+        df.to_csv("result.csv", mode='a', index=False)  ##### ⚠⚠⚠ 注意写入模式 ! 建议每个函数后都单独调用 save_file函数 及时释放内存
+        del df
         # 如果性能不佳 请使用  del df   手动回收内存
