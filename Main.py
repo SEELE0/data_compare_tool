854a9493080966eeaef0eba041e8e0423f790197
SEELE0 <61261918+SEELE0@users.noreply.github.com>
2025-04-02 08:46:58 +0000

    优化打印输出日志 & 后处理逻辑

--- Main.py
+++ Main.py
@@ -6,6 +6,17 @@ from DataSource.Sourcefactory import Sourcefactory
 from MainActuator import MainActuator
 from PostProcessing import PostProcessing
 import pandas as pd
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,  # 设置最低日志级别
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # 设置日志格式
+    handlers=[
+        # logging.FileHandler('app.log'),  # 将日志记录到文件中
+        logging.StreamHandler()  # 将日志输出到控制台
+    ]
+)
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     # config_path = './Config/config.json'
@@ -25,10 +36,10 @@ if __name__ == '__main__':
 
     config_reader = ConfigReader(config_path, config_type)
     config_data = config_reader.config_data
-
+    logger.info("读取配置文件成功")
     Datafactory = Sourcefactory()
     # config_data = ConfigReader('config_path').config_data
-    for config_row in config_data:
+    for index,config_row in enumerate(config_data):
         df1, df2 = Datafactory.filter_same_colname(
             Datafactory.create_entity(config_row['UpStream_Type'],
                                       config_row['TableName_Upstream'],
@@ -37,7 +48,8 @@ if __name__ == '__main__':
                                       config_row['TableName_DownStream'],
                                       config_row['filter']).fillna('Null')
         )
-
+        logger.info(f"================={index+1} - 读取数据成功=================")
+        logger.info(f"当前处理数据配置为  {config_row}")
         find_different_records = MainActuator.find_different_records(df1, df2, key_columns=config_row['key'])
 
         # 方案一写法
@@ -60,9 +72,13 @@ if __name__ == '__main__':
         # extra_records_df = MainActuator.find_extra_records(df1, df2, key_columns=config_row['key'])
 
         # 这里开始 进入后处理
-        PostProcessing.process_diff_records(find_different_records, config_row['key'], 'test_upstream.csv',
-                                            'test_downstream.csv')
+        logger.info(f"================= 数据比对完成 =================")
+        logger.info(f"================= 开始输出结果 =================")
         PostProcessing.process_missing_extract_records(filtered_missing_df, 'missing_records',
-                                                       'test_upstream.csv')
+                                                       config_row['TableName_DownStream'])
         PostProcessing.process_missing_extract_records(filtered_extra_df, 'extra_records',
-                                                       'test_downstream.csv')
+                                                       config_row['TableName_DownStream'])
+        PostProcessing.process_diff_records(find_different_records, config_row['key'], config_row['TableName_Upstream'],
+                                            config_row['TableName_DownStream'])
+        logger.info(f"================= 结果输出完成 =================")
+
